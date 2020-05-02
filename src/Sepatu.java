18f8227000bc23d7262342a76286ec0a92f0556e
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-05-02 12:09:04 +0800

    Sepatu

---
+++ src/Sepatu.java
@@ -0,0 +1,21 @@
+public class Sepatu {
+    //attribute
+    String merk;
+    String jenis;
+    String warna;
+    int nomor;
+    double harga;
+
+    //behavior
+    void cetakInfo(){
+        System.out.println("Merk :" + merk);
+        System.out.println("Jenis :" + jenis);
+        System.out.println("Warna :" + warna);
+        System.out.println("Ukuran :" + nomor);
+        System.out.println("Harga :" + harga);
+    }
+
+    void membelisepatuyangdisukai(){
+
+    }
+}
